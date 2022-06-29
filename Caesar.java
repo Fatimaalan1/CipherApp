7e9de3118600a383407d2fd6d3c18427a8216463
Fatima Ahmed <94482546+fatimaAhmed36@users.noreply.github.com>
2022-06-28 19:02:42 -0600

    cont

--- Caesar.java
+++ Caesar.java
@@ -1,13 +1,8 @@
-import java.util.Scanner;
-
-//import java.io.*;
-//import java.util.*;
-
-
 public class Caesar {
 	public static final String alpha = "abcdefghijklmnopqrstuvwxyz";
 	
-	public static String encrypt(String text, int shift) {
+	
+	public static String encryptCaesar(String text, int shift) {
 		String Cipher = "";
 		if (shift > 26){
 			shift = shift % 26;
@@ -45,11 +40,43 @@ public class Caesar {
 		return Cipher;
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner myScanner = new Scanner(System.in);
-		String message = new String();
-		
-	}
+	
+	public static String decryptCaesar(String text, int shift) {
+		String Cipher = "";
+		if (shift > 26){
+			shift = shift % 26;
+		}
+		// TODO Auto-generated constructor stub
+		else if (shift < 0){
 
+		}
+		int length = text.length();
+		for (int i = 0; i < length; i++){
+			char x = text.charAt(i);
+			if (Character.isLetter(x));
+				if (Character.isLowerCase(x)){
+					char shifted_x = (char)(x - shift);
+					if (shifted_x<'a'){
+						Cipher += (char)(x + (26 - shift));
+					}
+					else{
+						Cipher += shifted_x;
+					}
+				}
+				else if (Character.isUpperCase(x)){
+					char shifted_x = (char)(x - shift);
+					if (shifted_x<'A'){
+						Cipher += (char)(x + (26 - shift));
+					}
+					else{
+						Cipher += shifted_x;
+					}
+				}else{
+			
+			Cipher += x;
+			}
+		}
+		return Cipher;
+	}
 }
+	
